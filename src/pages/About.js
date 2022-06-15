a4d23fda7bb1a8da552bd7d6ce8cbaaee6864ead
Elizabeth Lee <elizabethlee245@gmail.com>
2022-06-15 15:25:23 -0500

    week 1 finished

--- src/pages/About.js
+++ src/pages/About.js
@@ -22,7 +22,7 @@ export default class About extends Component {
               Hello, Elizabeth here! I'm a rising junior at Carnegie Mellon University
               studying Information Systems, Human-Computer Interaction, and Computer Science.
               Outside classes, I like to play the piano and drive for my CMU Buggy team.
-              This summer, I'm a Product Management intern for HP Inc in Houston, TX!
+              This summer, I'm a Product Management intern for HP Inc. in Houston, TX!
             </div>
           </div>
         </div>
