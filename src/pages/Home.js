1e983401e61628bf38c05b98e31dd8defe3acd6b
Elizabeth Lee <elizabethlee245@gmail.com>
2022-06-15 15:24:58 -0500

    week1 finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-  
+import AddTodo from "../component/AddTodo.js"; 
+import Todos from "../component/todos.js"; 
+import "./Home.css";
+
+
 class Home extends Component {
   // A default state of this component with an empty list of todos.
   constructor() {
     super();
     this.state = {
       // create your empty list here call it todos.
+      todos: []
     };
   }
+
+
+
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
@@ -24,11 +32,17 @@ class Home extends Component {
       todos: new_list,
     });
   };
+
+
+
   render() {
     return (
       <div className="Home">
         <h1>Todo's </h1>
-        <p> Replace this</p>
+        <p> 
+          <Todos todos={ this.state.todos } />
+          <AddTodo addTodo={ this.addTodo } />
+        </p>
       </div>
     );
   }
